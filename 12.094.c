76bdfcfc89b3e04bbc73e348d6181d6387e117b2
266844938 <56297593+266844938@users.noreply.github.com>
2019-12-09 09:34:13 +0800

    Add files via upload

---
+++ 12.094.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int isleapYear(int x)
+{
+    int i;
+    for (i = 2000; i<= 2050; i++)
+    {
+        if ((i % 4==0 && i % 400!=0)||(i % 400==0)){     
+            return 1;
+        }else{
+             return 0;
+        }
+    }
+   
+}
+int main(void)
+{
+ int i, flag,count=0;
+    for (i = 2000; i <= 2050; i++)
+    {
+        flag = isleapYear(i);
+        if (flag == 1)
+        {
+            printf("%5d", i);
+           count++;
+        }
+    } 
+     printf("%5d", count);
+    return 0;
+}
